d65fb41fd9ed291e3b90906b6dfbd48b015d295a
Nahl <nahlultra@gmail.com>
2021-04-02 16:49:57 +0700

    init

---
+++ lib/screen/home.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: DefaultTextStyle(
+          style: GoogleFonts.workSans(color: Colors.black),
+          child: ListView(
+            padding: EdgeInsets.only(right: 24, left: 24, top: 64, bottom: 0),
+            children: [
+              Text("Juwairiyah binti Al-Harist",
+                  style: GoogleFonts.workSans(
+                      fontWeight: FontWeight.bold, fontSize: 42)),
+              Text("Biografi", style: TextStyle(fontSize: 24)),
+              SizedBox(height: 64),
+              Text(
+                  '''Kata Ibunda Aisyah radhiallahu ‘anha, “Juwairiyah adalah seorang wanita yang cantik dan memesona. Tidak seorang pun yang melihatnya, pasti jatuh hati padanya.”
+
+Nasabnya
+
+Ummul mukminin Juwairiyah binti al-Harits adalah seorang bangsawan dari bani Musthaliq. Nama dan nasabnya adalah Juwairiyah binti al-Harits bin Abi Dhirar bin Habib bin ‘A-idz bin Malik bin Jadzimah. Jadzimah inilah yang disebut Musthaliq. Ia berasal dari kabilah Khuza’ah. Nabi menikahinya usai Perang Muraisi’. Yaitu peperangan antaran kaum muslimin dengan Bani Musthaliq yang terjadi pada tahun ke-5 H. Ada pula yang mengatakan tahun ke-6 H. Sebelum menikah dengan Rasulullah, ia merupakan istri dari Musafi’ bin Shafwan al-Mushthaliq.
+
+Ibnu Ishaq mengatakan, “Juwairiyah binti al-Harits, dulu namanya adalah Barrah binti al-Harits bin Abi Dhirar bin Habib bin ‘A-idz bin Malik bin Jadzimah dari kabilah Khuza’ah. Ia merupakan istri dari anak pamannya, Musafi’ bin Shafwan bin Dzi al-Syafr.
+
+Dari Zainab binti Abu Salamah dari Juwairiyah binti al-Harits, ia menceritakan bahwa namanya dahulu adalah Barrah. Nabi mengganti namanya menjadi Juwairiyah. Alasannya karena beliau tidak suka kalau sehabis berjumpa dengannya dikatakan, “Beliau keluar dari Barrah (kebaikan).” Riwayat ini sesuai dengan syarat Muslim, walaupun ia tidak meriwayatkannya.
+
+Di Masa Jahiliyah
+
+Ummul mukminin Juwairiyah adalah tokoh di tengah kaumnya. Ayahnya adalah kepala kabilah Bani Musthaliq. Sampai kabar kepada Rasulullah bahwa Bani Musthaliq telah sepakat untuk menyerang beliau dan dipimpin oleh al-Harits bin Abi Dhirar. Kabar tersebut segera direspon oleh Rasulullah. Beliau menyiapkan pasukan dan keluar untuk menghadapi mereka. Kedua pasukan bertemu di sebuah mata air yang disebut al-Muraisi’. Bani Musthaliq berhasil dikalahkan. Al-Harits bin Abi Dhirar tewas dalam perang tersebut. Imbasnya, kaum wanita dan anak-anak Bani Musthaliq menjadi tawanan. Mereka diserahkan kepada para sahabat. Di antara tawanan tersebut terdapat Juwairiyah binti al-Harits, tokoh wanita Bani Musthaliq.
+
+Memeluk Islam
+
+Abdullah bin Umar mengatakan, “Muhammad bin Yazid mengabarkan kepadaku dari neneknya. Neneknya adalah budak dari Juwairiyah binti al-Harits. Bahwa Juwairiyah radhiallahu ‘anha berkata, “Rasuullah menikahiku saat aku berusia 20 tahun.” Neneknya mengatakan, “Juwairiyah wafat pada tahun 50 H. Saat itu beliau berusia 65 tahun. Dan yang menjadi imam shalatnya adalah Khalifah Marwan bin al-Hakam.”
+
+Usai perang Bani Musthaliq, Juwairiyah binti al-Harits menjadi tawanan perang. Suaminya terbunuh dalam perang ini. Ia berada di tangan sahabat Tsabit bin Qais bin asy-Syammas radhiallahu ‘anhu. Menjadi tawanan, tidak membuat Juwairiyah nyaman. Ia pun ingin menebus dirinya agar bebas. Karena ia adalah seorang tokoh dari kaumnya. Namun, ia tak memiliki apapun yang bisa digunakan untuk menebus dirinya. Lalu ia pergi menuju Rasulullah shallallahu ‘alaihi wa sallam. Berharap agar beliau mau membantu. Ternyata Rasulullah memberikan tawaran yang jauh lebh baik dan lebih utama dari apa yang ia inginkan. Nabi melamarnya dan menanggung pembebasannya. Juwairiyah menerima tawaran tersebut dan sekaligus memeluk Islam.
+
+Wanita Yang Banyak Keberkahannya
+
+Aisyah radhiallahu ‘anha menceritakan, “Saat Rasulullah menawan orang-orang Bani Musthaliq, Juwairiyah binti al-Harits berada pada tangan sahabat Tsabit bin Qays bin asy-Syammas radhiallahu ‘anhu atau pada anak pamannya. Juwairiyah berkeinginan membebaskan dirinya. Ia adalah seorang wanita yang cantik dan memesona. Hampir-hampir tak ada seorang pun yang melihatnya kecuali jatuh hati padanya. Ia datang menemui Rasulullah, dengan maksud meminta tolong kepada beliau untuk membebaskan dirinya. Demi Allah, tatkala aku melihat ia berdiri di depan pintu rumahku, aku tidak menyukai hal itu. Karena aku tahu, Rasulullah akan melihat apa yang aku lihat (melihat kecantikannya pen.).
+
+Juwairiyah berkata, ‘Hai Rasulullah, aku memiliki masalah yang telah Anda ketahui. Aku ingin membebaskan diriku. Dan aku datang kepadamu agar kau menolongku’. Rasulullah menanggapi, ‘Atau kau ingin yang lebih baik dari itu?’ ‘Apa itu, Rasulullah?’ tanya Juwairiyah. Rasulullah berkata, ‘Aku menikahimu dan kutunaikan pembebasanmu’. Juwairiyah menjawab, ‘Tentu mau’. Nabi menjawab, ‘Aku telah melakukannya’.”
+
+Aisyah melanjutkan, “Kabar ini pun tersebar di tengah kaum muslimin. Mereka berkata, ‘(tawanan kita ini) ipar-ipar Rasulullah!’. Mereka pun membebaskan semua tawanan Bani Musthaliq. Tawanan yang dibebaskan karena pernikahan Juwairiyah ini berjumlah 100 orang Bani Musthaliq. Aku tidak mengetahui wanita yang paling besar berkahnya terhadap kaumnya dibanding Juwairiyah.”
+
+Kisah Ibunda Juwairiyah ini sungguh menarik. Kita semakin sadar bahwa hati manusia itu dalam genggaman Allah. Dan hidayah itu benar-benar milik Allah. Juwairiyah sebelumnya adalah wanita kafir yang menjadi tawanan (menjadi budak). Kedudukan yang hina di dunia dan akhirat. Kemudian ia memeluk Islam dan menikahi manusia terbaik yang pernah ada. Statusnya berubah menjadi wanita mulia di dunia dan kedudukan yang tinggi di akhirat. Dalam sekejap saja keadaan tersebut berubah. Berbalik dari keadaan paling hina menjadi amat sangat mulia. Menjadi ibu dari semua orang-orang yang beriman.
+
+Kemudian keberkahan dirinya dan pernikahannya juga dirasakan oleh sejumlah besar kaumnya. Mereka semua terbebas. Setelah sebelumnya menjadi tawanan.
+
+Pernikahan Ummul Mukminin Juwairiyah dengan Nabi Muhammad shallallahu ‘alaihi wa sallam juga membantah perkataan orang-orang yang berlebihan yang mengatakan bahwa Nabi hanya menikahi janda-janda tua saja.
+
+Bimbingan Rasulullah
+
+Rasulullah adalah seorang suami yang pandai menanamkan pengaruh kepada manusia. Dan tentu saja terhadap istrinya. Setelah memeluk Islam, Juwairiyah menjadi seorang wanita yang rajin beribadah. Ia banyak mengerjakan puasa sunnah. Saat ia berpuasa sunnah di hari Jumat saja, Nabi perintahkan dia untuk membatalkannya. Di antara kebiasaannya adalah berdzikir mulai dari usai shalat subuh hingga terbit matahari.
+
+Dari Abu Ayyub dari Juwairiyah binti al-Harits radhiallahu ‘anha, Nabi shallallahu ‘alaihi wa sallam pernah menemuinya di hari Jumat. Saat itu Juwairiyah sedang berpuasa. Nabi bertanya, “Apakah kau kemarin berpuasa?” Juwairiyah menjawab, “Tidak.” Nabi melanjutkan, “Besok kau akan berpuasa?” “Tidak”, jawab Juwairiyah. “Jika demikian, batalkanlah”, kata Nabi shallallahu ‘alaihi wa sallam.
+
+Dari Ibnu Abbas, dari Ibunda Juwairiyah yang nama aslinya adalah Barrah. Namun Nabi menggantinya menjadi Juwairiyah, karena beliau tidak suka kalau dikatakan, ‘Beliau keluar dari Barrah (kebaikan)’. Juwairiyah berkata, “Nabi keluar dari rumahku. Saat itu aku sedang berada di mushalla rumahku. Beliau kembali lagi saat siang, sementara aku masih di tempat itu. Beliau berkata, ‘Engkau tidak meninggalkan mushallamu sedari aku keluar tadi?’ ‘Iya’, jawabku. Beliau bersabda,
+
+لَقَدْ قُلْتُ بَعْدَكِ أرْبَعَ كَلِمَاتٍ ثَلاثَ مَرَّاتٍ ، لَوْ وُزِنَتْ بِمَا قُلْتِ مُنْذُ اليَوْمِ لَوَزَنَتْهُنَّ : سُبْحَانَ الله وَبِحَمْدِهِ عَدَدَ خَلْقِهِ ، وَرِضَا نَفْسِهِ ، وَزِنَةَ عَرْشِهِ ، وَمِدَادَ كَلِمَاتِهِ
+
+Sungguh, aku telah mengucapkan setelahmu empat kalimat sebanyak tiga kali. Jika ditimbang dengan yang kau ucapkan sejak tadi tentu akan menyamai timbangannya yaitu, “SUBHAANALLOHI WA BI-HAMDIH, ‘ADADA KHOLQIH, WA RIDHOO NAFSIH, WA ZINATA ‘ARSYIH, WA MIDAADA KALIMAATIH. (artinya: Mahasuci Allah. Aku memuji-Nya sebanyak makhluk-Nya, sejauh kerelaan-Nya, seberat timbangan ‘Arsy-Nya, dan sebanyak tinta tulisan kalimat-Nya).” (HR. Muslim, no. 2726).
+
+Dari ath-Thufail putra dari saudara laki-laki Juwairiyah, dari Juwairiyah bahwa Nabi shallallahu ‘alaihi wa sallam bersabda,
+
+منْ لَبِسَ الحَرِيرَ في الدُّنْيا لَمْ يَلْبسْهُ في الآخرَةِ
+
+“Seorang laki-laki yang memakai sutra di dunia, dia tidak akan memakainya di akhirat.” (Muttafaqun ‘alaih).
+
+Wafat
+
+Ibnu Umar mengatakan, “Abdullah bin Abu al-Abyadh bercerita bahwa ayahnya menyampaikan, ‘Juwairiyah binti al-Harits, istri Nabi, wafat di bulan Rabiul Awal tahun 56 H. Ia wafat di masa pemerintah Muawiyah. Yang menjadi imam shalat jenazahnya adalah Marwan bin al-Hakam yang saat itu menjabat gubernur Madinah’.”
+
+Dalam riwayat yang berbeda, Abdullah bin Umar berkata, “Aku dikabari oleh Muhammad bin Yazid dari neneknya yang merupakan budak dari Juwairiyah binti al-Harits bahwa Juwairiyah radhiallahu ‘anha berkata, ‘Rasulullah menikahiku saat aku berusia 20 tahun’.” Neneknya mengatakan, “Juwairiyah wafat pada tahun 50 H. Saat itu usianya 65 tahun. Imam shalat jenazahnya adalah Marwan bin al-Hakam.
+
+Pendapat yang lebih tepat adalah Ummul Mukminin Juwairiyah binti al-Harits wafat di bulan Rabiul Awal tahun 56 H, Allahu a’lam.
+
+Sumber:
+– https://islamstory.com/ar/artical/81/أم_المؤمنين_جويرية_بنت_الحارث
+
+
+
+Read more https://kisahmuslim.com/6178-ummul-mukminin-juwairiyah-binti-al-harits.html'''),
+              SizedBox(
+                height: 42,
+              ),
+              Wrap(
+                alignment: WrapAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Container(
+                      color: Color.fromRGBO(0, 0, 0, 0.04),
+                      padding: EdgeInsets.only(
+                          right: 12, left: 12, top: 8, bottom: 8),
+                      child: Text(
+                        "nahlmb.dev - 2020 - IDN Final Task\nsumber : kisahmuslim.com",
+                        style: TextStyle(color: Colors.black54),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 42,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
